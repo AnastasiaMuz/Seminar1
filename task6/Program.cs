eab1e07aadcacc08123e24c5dceacbd8d25be162
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-08 09:15:26 +0300

    add task6

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите трехзначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+
+if (number >=100 & number < 1000)
+{
+    Console.Write (number % 10);
+}
+
+else
+{
+    Console.Write ("Это число не трехначное.");
+}
